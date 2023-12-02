db323e86d28c4e9ff27a08fe75a52438630a8b98
Ierzi <ierziytb@gmail.com>
2023-12-02 20:39:40 +0100

    #1: No comment

--- script.js
+++
@@ -1,7 +0,0 @@
-const textarea = document.getElementById('userInput');
-
-textarea.addEventListener('input', (event) => {
-    const enteredText = event.target.value;
-    // Add your logic to handle the entered text
-    console.log('Entered Text:', enteredText);
-});
